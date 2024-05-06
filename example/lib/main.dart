4d318abc68f8e31ab30093033780a0f4fcf5bcbb
janu1499 <januja@apptimustech.com>
2024-05-06 12:31:49 +0530

    example create

---
+++ example/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:languagehandling/languagehandling.dart';
+
+import 'homepage.dart';
+import 'localization/locale.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final LanguageHandling localization = LanguageHandling.instance;
+
+  @override
+  void initState() {
+    configureLocalization();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Language Testing',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+      supportedLocales: localization.supportedLocales,
+      localizationsDelegates: localization.localizationsDelegates,
+      home: const Homepage(),
+    );
+  }
+
+  void configureLocalization() {
+    localization.init(
+        mapLocales: LOCALES, initLanguageCode: "en"); // Pass LOCALES here
+    localization.onTranslatedLanguage = onTranslatedLanguage;
+  }
+
+  void onTranslatedLanguage(Locale? locale) {
+    setState(() {});
+  }
+}
